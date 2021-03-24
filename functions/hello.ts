19bd62fab183c076cc44e31dacc227ec882632d7
Blake Mealey <blakemealey@gmail.com>
2021-03-23 18:11:09 -0600

    improved types

--- functions/hello.ts
+++ functions/hello.ts
@@ -1,10 +1,14 @@
 import { createHandler } from './util/create-handler';
 
-export const handler = createHandler(async () => {
+interface Message {
+  message: string;
+}
+
+export const handler = createHandler<any, any, Message>(async () => {
   return {
     statusCode: 200,
     body: {
-      message: 'super hello',
+      message: 'typed response',
     },
   };
 });
--- functions/util/create-handler.ts
+++ functions/util/create-handler.ts
@@ -1,42 +1,51 @@
 import { Context, Handler } from 'aws-lambda';
 
-interface RawEvent {
+type StatusCode = 200;
+type HttpMethod = 'POST' | 'PUT' | 'PATCH' | 'GET' | 'DELETE';
+type HeaderKeys = 'Authorization' | 'Content-Type';
+
+interface CommonEvent<TParamKeys extends string = string> {
   path: string;
-  httpMethod: 'POST' | 'PUT' | 'PATCH' | 'GET' | 'DELETE';
-  headers: Record<string, string>;
-  queryStringParameters: Record<string, string>;
-  body?: string;
+  httpMethod: HttpMethod;
+  headers: Record<HeaderKeys, string>;
+  queryStringParameters: Record<TParamKeys, string>;
   isBase64Encoded: boolean;
 }
 
-interface Event<TBody> {
-  path: string;
-  httpMethod: 'POST' | 'PUT' | 'PATCH' | 'GET' | 'DELETE';
-  headers: Record<string, string>;
-  queryStringParameters: Record<string, string>;
-  body?: TBody;
-  isBase64Encoded: boolean;
-}
-
-interface RawResponse {
-  statusCode: 200;
+interface RawEvent extends CommonEvent {
   body?: string;
 }
 
-interface Response<TBody> {
-  statusCode: 200;
+interface Event<TBody, TParamKeys extends string>
+  extends CommonEvent<TParamKeys> {
   body?: TBody;
 }
 
-export function createHandler<TEventBody = any, TResponseBody = any>(
+interface CommonResponse {
+  statusCode: StatusCode;
+}
+
+interface RawResponse extends CommonResponse {
+  body?: string;
+}
+
+interface Response<TBody> extends CommonResponse {
+  body?: TBody;
+}
+
+export function createHandler<
+  TEventBody = any,
+  TParamKeys extends string = string,
+  TResponseBody = any
+>(
   func: (
-    event: Event<TEventBody>,
+    event: Event<TEventBody, TParamKeys>,
     context: Context
   ) => Promise<Response<TResponseBody>>
 ) {
   let handler: Handler<RawEvent, RawResponse> = async (rawEvent, context) => {
     const eventBody = rawEvent.body ? JSON.parse(rawEvent.body) : undefined;
-    const event: Event<TEventBody> = {
+    const event: Event<TEventBody, TParamKeys> = {
       ...rawEvent,
       body: eventBody,
     };
